abe7c889ba2112deaa81f0f470d9bd0c7deb9d75
Alan Yin <ayin@darwinium.com>
2023-09-18 15:05:57 +1000

    update

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "Calculator", path: "./Calculator-framework.xcframework.zip"),
+        .binaryTarget(name: "Calculator", path: "./Calculator-framework.xcframework"),
         //Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         //.target(
