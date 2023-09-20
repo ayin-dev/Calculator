9674d220c8870dd9afa61b2c2312a52fcb8fd90c
Alan Yin <ayin@darwinium.com>
2023-09-20 17:31:56 +1000

    update

--- Package.swift
+++ Package.swift
@@ -9,14 +9,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Calculator",
-            targets: ["Calculator"]),
+            targets: ["Calculator", "Logic"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        //.binaryTarget(name: "Calculator", path: "./Logic_Framework.xcframework"),
+        .binaryTarget(name: "Logic", path: "./Logic_Framework.xcframework"),
         .binaryTarget(name: "Calculator", path: "./Calculator-framework.xcframework"),
         //Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
