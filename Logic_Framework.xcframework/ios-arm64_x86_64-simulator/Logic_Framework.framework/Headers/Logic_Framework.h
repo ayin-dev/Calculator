2903deedd73adf5ed095dc5008a575689aad18f3
Alan Yin <ayin@darwinium.com>
2023-09-20 17:13:08 +1000

    update

---
+++ Logic_Framework.xcframework/ios-arm64_x86_64-simulator/Logic_Framework.framework/Headers/Logic_Framework.h
@@ -0,0 +1,18 @@
+//
+//  Logic_Framework.h
+//  Logic_Framework
+//
+//  Created by Alan Yin on 20/9/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Logic_Framework.
+FOUNDATION_EXPORT double Logic_FrameworkVersionNumber;
+
+//! Project version string for Logic_Framework.
+FOUNDATION_EXPORT const unsigned char Logic_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logic_Framework/PublicHeader.h>
+
+
